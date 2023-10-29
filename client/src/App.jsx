bd7191ebe032f746f43a9b99df2e365d9c6372fe
DaniBacheva <y.bacheva@abv.bg>
2023-10-29 22:01:33 +0200

    Add components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,25 @@
 
-
+import './App.css'
 import { useState } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Search } from './components/Search'
+import { UserList } from './components/UserList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-  
-     <>
-   <Header />
-
-
-   <Footer />
-   </>
+    <>
+      <Header />
+      <main>
+      <section class="card users-container">
+        <Search />
+<UserList />
+</section>
+      </main>
+      <Footer />
+    </>
   )
 }
 
